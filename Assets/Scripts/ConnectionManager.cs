fed969b9b77bc2b422ba80816665ca364c94a85a
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-07-25 00:20:22 +0900

    Read csv data

---
+++ Assets/Scripts/ConnectionManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+
+public class ConnectionManager : SingletonMonoBehaviour<ConnectionManager> {
+
+	public void ConnectionAPI(string url, UnityAction<string> onFinish = null, UnityAction<string> errorFinish = null){
+		StartCoroutine (ConnectionAPICoroutine (url, onFinish, errorFinish));
+	}
+
+	private IEnumerator ConnectionAPICoroutine(string url, UnityAction<string> onFinish = null, UnityAction<string> errorFinish = null){
+		var request = UnityWebRequest.Get (url);
+		GameManager.Log ("通信開始 : " + url);
+		yield return request.Send ();
+		if (!request.isError) {
+			GameManager.Log ("url:" + url + "\nSuccess : " + request.downloadHandler.text);
+			if (onFinish != null) {
+				onFinish (request.downloadHandler.text);
+			}
+		} else {
+			GameManager.Log ("url:" + url + "\nFaild : " + request.error);
+			if (errorFinish != null) {
+				errorFinish (request.error);
+			}
+		}
+	}
+}
