b768fc78b349b10c3aa804a395aa7751edd60e7c
Yuuki Tsuzuki <nagarebo.tsuzuki@gmail.com>
2017-07-25 21:33:09 +0900

    Create UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager
-	: SingletonMonoBehaviour<GameManager>
-{
-	private void Start()
-	{
-		MasterDataManager.instance.LoadData(() => 
-			{
-				print("ロード終わったお");
-			});
+public class GameManager : SingletonMonoBehaviour<GameManager> {
+	
+	private void Start() {
+		MasterDataManager.instance.LoadData(() => {
+			print("ロード終わったお");
+		});
 	}
 
-	public static void Log (object log)
-	{
-		if (Debug.isDebugBuild)
-		{
+	public static void Log (object log) {
+		if (Debug.isDebugBuild) {
 			Debug.Log(log);
 		}
 	}
